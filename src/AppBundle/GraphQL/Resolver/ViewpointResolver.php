60a8f2b044fe5cf9fb9c9e5625bdd776eb654eee
Vassyli <basilius.sauter@gmail.com>
2016-09-02 15:27:08 +0200

    Adds village module as dependency to provide a default scene.

--- src/AppBundle/GraphQL/Resolver/ViewpointResolver.php
+++ src/AppBundle/GraphQL/Resolver/ViewpointResolver.php
@@ -7,6 +7,7 @@ use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerAwareTrait;
 use Overblog\GraphQLBundle\Definition\Argument;
 
+use LotGD\Core\Exceptions\InvalidConfigurationException;
 use LotGD\Crate\GraphQL\Services\BaseManagerService;
 
 class ViewpointResolver extends BaseManagerService implements ContainerAwareInterface
@@ -20,15 +21,27 @@ class ViewpointResolver extends BaseManagerService implements ContainerAwareInte
         }
 
         $characterId = (int)$args["characterId"];
+        /* @var LotGD\Core\Models\Character */
         $character = $this->container->get("lotgd.crate.graphql.character_manager")->findById($characterId);
 
         if (is_null($character)) {
+            // Character not found, return null
             return null;
         } else {
-            return [
-                "title" => "Hallo",
-                "description" => "Hallo.",
-            ];
+            // @ToDo Return null if user has no access rights to this character.
+            $game = $this->getGame();
+            $game->setCharacter($character);
+
+            try {
+                $viewpoint = $game->getViewpoint();
+
+                return [
+                    "title" => "Hallo",
+                    "description" => "Hallo.",
+                ];
+            } catch (InvalidConfigurationException $e) {
+                throw new \Overblog\GraphQLBundle\Error\UserError("No default scene!");
+            }
         }
     }
 }
