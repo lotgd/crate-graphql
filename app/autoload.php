3f2c127455d643bd3aa2c15e5d17b1823ea6fe0f
Vassyli <basilius.sauter@gmail.com>
2018-01-18 11:10:52 +0100

    autoload now tries to load from somewhere else if normal loading fails.

--- app/autoload.php
+++ app/autoload.php
@@ -6,7 +6,14 @@ use Composer\Autoload\ClassLoader;
 /**
  * @var ClassLoader $loader
  */
-$loader = require __DIR__.'/../vendor/autoload.php';
+$loader = null;
+$autoloader = __DIR__.'/../vendor/autoload.php';
+
+if (file_exists($autoloader)) {
+    $loader = require $autoloader;
+} else {
+    $loader = require getcwd() . "/vendor/autoload.php";
+}
 
 AnnotationRegistry::registerLoader([$loader, 'loadClass']);
 
